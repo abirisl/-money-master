b737cdd7379a9f78777b1792ddd91191ae6da91d
abir islam <mhabir979292@gmail.com>
2022-02-17 18:00:12 +0600

    first commit

---
+++ js/Money.js
@@ -0,0 +1,64 @@
+// all input value item
+
+function inputBtn(total){
+    const totalCost= document.getElementById(total +'-input');
+    const totalValue= totalCost.value;
+    return totalValue;
+}
+// total Amount item
+
+function totalAmount(amount,totalCostAmount,){
+    const totalcost= document.getElementById(amount+'-total');
+    let totalCostInner= totalcost.innerText;
+    totalcost.innerText=totalCostAmount;
+    totalCostAmount= totalCostInner;
+}
+
+
+
+// calculate button.......
+
+document.getElementById('btn').addEventListener('click', function totalBalanceValue(){
+
+     const foodCost= inputBtn('food');
+    const rentCost= inputBtn('rent');
+    const colthCost= inputBtn('cloth');
+    const totalCostAmount=parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(colthCost);
+    const incomeInput= inputBtn('income')
+    const totalBalance= parseFloat(incomeInput) - totalCostAmount;
+    const errorMessage= document.getElementById('error-msg')
+    if(totalCostAmount>0 ){
+        totalAmount('expens',totalCostAmount);   
+        
+    }
+    if(totalBalance>0){
+        errorMessage.style.display='none'
+       
+    }
+    else{
+        errorMessage.style.display='block'
+    }
+    
+   
+})
+
+// save button.......
+
+document.getElementById('save-btn').addEventListener('click', function(){
+
+    const incomeInput= inputBtn('income');
+    const saveInput= inputBtn('save');
+    const saveTotal= parseFloat(incomeInput) * parseFloat(saveInput) / 100;
+
+    if(saveTotal>0){
+        
+    totalAmount('save',saveTotal);
+    }
+
+
+    const balanceTotal= document.getElementById('balance-total').innerText;
+    const saveTotalAmount= document.getElementById('save-total').innerText;
+    let remainingTotal= balanceTotal-saveTotalAmount;
+    totalAmount('remaining', remainingTotal);
+   
+})
